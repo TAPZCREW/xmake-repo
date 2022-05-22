cc791b11141adfee2ca57305a2d173c4aeab1af6
Arthur <arthur.laurent4@gmail.com>
2022-05-22 17:16:09 +0200

    Add stormkit to repo
    Fix stormkit xmake.lua
    
    
    Fix stormkit branch
    
    
    Pass package configs directly
    
    
    Revert last commit
    
    
    .
    
    
    .
    
    
    .
    
    
    .
    
    
    Add arthapz patched robin-hood-hashing
    
    
    Add nazarautils
    
    
    update nzsl
    
    
    Update nzsl
    
    
    zs
    
    
    f

---
+++ packages/s/stormkit/xmake.lua
@@ -0,0 +1,28 @@
+package("stormkit")
+
+    set_homepage("https://gitlab.com/Arthapz/stormkit")
+
+    set_urls("https://gitlab.com/Arthapz/stormkit.git")
+    add_versions("latest", "main")
+
+    add_configs("enable_wsi", {description = "Enable WSI support", default = true, type = "boolean"})
+
+    if is_plat("linux") then
+        add_configs("enable_wsi_x11", {description = "Enable X11 WSI support", default = true, type = "boolean"})
+        add_configs("enable_wsi_wayland", {description = "Enable Wayland WSI support", default = true, type = "boolean"})
+    end
+
+    on_install(function(package)
+    
+        local configs = {}
+        configs.kind = package:config("shared") and "shared" or "static"
+        configs.enable_wsi = package:config("enable_wsi")
+
+        if is_plat("linux") then
+            configs.enable_wsi_x11 = package:config("enable_wsi_x11")
+            configs.enable_wsi_wayland = package:config("enable_wsi_wayland")
+        end
+
+        import("package.tools.xmake").install(package, configs)
+
+    end)
