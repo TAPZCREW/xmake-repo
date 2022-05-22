d0f289f84a4d9985ed48009a64e799ea897d52e1
Arthur <arthur.laurent4@gmail.com>
2022-05-22 23:09:22 +0200

    Add unity_build option to stormkit package

--- packages/s/stormkit/xmake.lua
+++ packages/s/stormkit/xmake.lua
@@ -6,6 +6,7 @@ package("stormkit")
     add_versions("latest", "main")
 
     add_configs("enable_wsi", {description = "Enable WSI support", default = true, type = "boolean"})
+    add_configs("unity_build", {description = "Enable Unity build", default = true, type = "boolean"})
 
     if is_plat("linux") then
         add_configs("enable_wsi_x11", {description = "Enable X11 WSI support", default = true, type = "boolean"})
@@ -17,6 +18,7 @@ package("stormkit")
         local configs = {}
         configs.kind = package:config("shared") and "shared" or "static"
         configs.enable_wsi = package:config("enable_wsi")
+        configs.unity_build = package:config("unity_build")
 
         if is_plat("linux") then
             configs.enable_wsi_x11 = package:config("enable_wsi_x11")
