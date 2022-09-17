08801ea34073119a535a7a335a94877b628b650b
Arthur LAURENT <arthur.laurent4@gmail.com>
2022-09-17 17:13:00 +0200

    Add cppfront

---
+++ packages/c/cppfront/xmake.lua
@@ -0,0 +1,23 @@
+package("cppfront")
+    set_homepage("https://github.com/hsutter/cppfront")
+    set_description("Herb Sutter's experimental C++ Syntax 2 -> Syntax 1 compiler")
+    set_license("CC-BY-NC-ND-4.0")
+
+    add_urls("https://github.com/hsutter/cppfront.git")
+
+    add_versions("2022.09.17", "8140ab920a38e9cc66d1862c9b823a98a9f25e3b")
+
+    on_install(function(package)
+        io.writefile("xmake.lua", [[
+            target("cppfront")
+                set_kind("binary")
+                add_files("source/*.cpp")
+                add_headerfiles("source/*.h")
+        ]])
+        import("package.tools.xmake").install(package, {})
+        package:addenv("PATH", "bin")
+    end)
+
+    on_test(function(package)
+        os.vrun("cppfront --help")
+    end)
