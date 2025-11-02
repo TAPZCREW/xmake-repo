b79631e99559069373cd2fff6310e7dc43d9be81
Arthur LAURENT <arthur.laurent4@gmail.com>
2025-11-02 02:30:09 +0100

    update

--- packages/u/unordered_dense/xmake.lua
+++ packages/u/unordered_dense/xmake.lua
@@ -1,26 +1,29 @@
 package("unordered_dense")
-    set_base("unordered_dense")
+set_base("unordered_dense")
 
-    set_urls("https://github.com/Arthapz/unordered_dense.git")
+set_urls("https://github.com/Arthapz/unordered_dense.git")
 
-    add_configs("modules", {description = "Build with C++23 std modules support.", default = false, type = "boolean"})
-    add_configs("std_import", {description = "Build with C++23 std modules support.", default = false, type = "boolean"})
-    add_configs("cpp", {description = "Build with C++23 std modules support.", default = "20", type = "string"})
+add_configs("modules", { description = "Build with C++23 std modules support.", default = false, type = "boolean" })
+add_configs("std_import", { description = "Build with C++23 std modules support.", default = false, type = "boolean" })
+add_configs("cpp", { description = "Build with C++23 std modules support.", default = "20", type = "string" })
 
-    add_versions("20250419", "b45d4094c202f012385fd43b066df6df258c0700")
+add_versions("20250419", "b45d4094c202f012385fd43b066df6df258c0700")
+add_versions("20251102", "6d57119efcba8a298ef4059ceb68217cf3780075")
 
-    on_load(function(package)
-        local cpp = package:config("cpp")
-        assert(cpp == "20" or cpp == "23" or cpp == "26" or cpp == "latest")
-    end)
-    
-    on_install(function (package)
-        if not package:config("modules") then
-            import("package.tools.cmake").install(package)
-            os.cp("include", package:installdir())
-        else
-            os.cp("src/ankerl.unordered_dense.cpp", "src/ankerl.unordered_dense.cppm")
-            io.writefile("xmake.lua", [[
+on_load(function(package)
+    local cpp = package:config("cpp")
+    assert(cpp == "20" or cpp == "23" or cpp == "26" or cpp == "latest")
+end)
+
+on_install(function(package)
+    if not package:config("modules") then
+        import("package.tools.cmake").install(package)
+        os.cp("include", package:installdir())
+    else
+        os.cp("src/ankerl.unordered_dense.cpp", "src/ankerl.unordered_dense.cppm")
+        io.writefile(
+            "xmake.lua",
+            [[
                 option("std_import", {default = true, defines = "ANKERL_UNORDERED_DENSE_USE_STD_IMPORT"})
                 option("cpp", {default = "20"})
                 target("unordered_dense")
@@ -30,8 +33,9 @@ package("unordered_dense")
                     add_includedirs("include")
                     add_files("src/**.cppm", {public = true})
                     add_options("std_import", "cpp")
-            ]])
-            local configs = {cpp = package:config("cpp"), std_import = package:config("std_import")}
-            import("package.tools.xmake").install(package, configs)
-        end
-    end)
+            ]]
+        )
+        local configs = { cpp = package:config("cpp"), std_import = package:config("std_import") }
+        import("package.tools.xmake").install(package, configs)
+    end
+end)
