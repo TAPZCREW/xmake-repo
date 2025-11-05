bf3b159d2e8512d76c042e87d39ec9412c374eb0
Arthur LAURENT <arthur.laurent4@gmail.com>
2025-11-05 22:34:15 +0100

    update stormkit

--- packages/s/stormkit/xmake.lua
+++ packages/s/stormkit/xmake.lua
@@ -26,7 +26,7 @@ package("stormkit", function()
 
     add_versions("20251102", "db479e8c916dbec739279cd7fab5db1b7b8acb41")
     add_versions("20251105", "85f6d4997bed98607ffc644c0cb278e4cb4ce8db")
-    add_versions("20251106", "4ef38e01346e794983c5f5fc11f0b5b9231d8e0c")
+    add_versions("20251106", "2d9510115eb6aef1c0f345d41a381267d2198a29")
 
     local components = {
         core = {
