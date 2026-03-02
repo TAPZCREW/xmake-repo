725be136fc435a4be210b2207cd92efec07f5bf7
Arthur LAURENT <arthur.laurent4@gmail.com>
2026-03-02 14:18:08 +0100

    update stormkit dev

--- packages/s/stormkit/xmake.lua
+++ packages/s/stormkit/xmake.lua
@@ -38,18 +38,16 @@ package("stormkit", function()
     add_versions("20260206", "a1559694da2401281e7a5a100d9fad9a2f0ad3e0")
     add_versions("20260208", "9a5180192045abab69e745d2a697191357387d63")
 
-    add_versions("dev", "6e9055633cd8c35a4ddff587c2b6c144e38899d5")
+    add_versions("dev", "3c59eb3504ba2bc9e245e5cf2cc80685196b8129")
 
     add_bindirs("bin")
     add_includedirs("include")
     add_linkdirs("lib")
 
     on_component("stormkit", function(package, component)
-        print("AAAAAAAAAAAAAAA", package:components())
         component:add("links", "stormkit")
 
         component:add("deps", "core")
-        component:add("deps", "main")
         for name, _ in pairs(package:components()) do
             if name ~= "stormkit" and name ~= "main" and name ~= "core" and package:config(name) then
                 component:add("deps", name)
