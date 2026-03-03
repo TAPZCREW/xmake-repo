60c7e5a1553d3a7248a9556403783b928577af63
Arthur LAURENT <arthur.laurent4@gmail.com>
2026-03-03 17:38:51 +0100

    update stormkit dev

--- packages/s/stormkit/xmake.lua
+++ packages/s/stormkit/xmake.lua
@@ -38,7 +38,7 @@ package("stormkit", function()
     add_versions("20260206", "a1559694da2401281e7a5a100d9fad9a2f0ad3e0")
     add_versions("20260208", "9a5180192045abab69e745d2a697191357387d63")
 
-    add_versions("dev", "733d3c3a6e8898cf1369de14779ffa90f3115a8d")
+    add_versions("dev", "d44538ba0107f026116f706301758df683423165")
 
     add_bindirs("bin")
     add_includedirs("include")
